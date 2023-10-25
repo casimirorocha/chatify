d0e43dd62faabe399a9dbcf7c3f597d43fceb02f
Casimiro Rocha <casimiroaf@gmail.com>
2023-10-25 20:32:03 -0300

    Comenta corpo do método src/ChatifyMessenger@getUserWithAvatar

--- src/ChatifyMessenger.php
+++ src/ChatifyMessenger.php
@@ -292,13 +292,14 @@ class ChatifyMessenger
      */
     public function getUserWithAvatar($user)
     {
-        if ($user->avatar == 'avatar.png' && config('chatify.gravatar.enabled')) {
+        /* if ($user->avatar == 'avatar.png' && config('chatify.gravatar.enabled')) {
             $imageSize = config('chatify.gravatar.image_size');
             $imageset = config('chatify.gravatar.imageset');
             $user->avatar = 'https://www.gravatar.com/avatar/' . md5(strtolower(trim($user->email))) . '?s=' . $imageSize . '&d=' . $imageset;
         } else {
             $user->avatar = self::getUserAvatarUrl($user->avatar);
-        }
+        } */
+
         return $user;
     }
 
